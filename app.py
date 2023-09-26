d2fed31370f35e4e36a3927f115d62f2053e80ae
Aswinn4272 <130442173+Aswinn4272@users.noreply.github.com>
2023-09-26 21:54:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 
+# give the api url 
 url = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22"
     
 response = requests.get(url)
